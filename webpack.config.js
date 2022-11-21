e41179af8159773c34aee5a892235f2b35dc1a94
ElizPN <pankevych.liza@gmail.com>
2022-11-21 11:38:31 +0100

    add output

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ import path from "path";
 export default {
   mode: "development",
   entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
 };
 
 let test = path.resolve("dist", "assets");
