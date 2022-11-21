4fdb48778accf8d7b9a62c56f720fc5894a559dd
ElizPN <pankevych.liza@gmail.com>
2022-11-21 18:59:04 +0100

    add clean webpack plagin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 import path from "path";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 export default {
   mode: "development",
   context: path.resolve("src"),
   entry: {
-    test1: ["./test1.js", "./test2.js"],
-    test2: "./test3.js",
+    testOne: ["./test1.js", "./test2.js"],
+    testTwo: "./test3.js",
   },
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve("dist"),
+    path: path.resolve("dist"), // necessary for clean-webpack-plugin
   },
+  plugins: [new CleanWebpackPlugin()],
 };
