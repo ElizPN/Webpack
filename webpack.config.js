7b57d0e0368ed5ff3931402a4fd4831d9febcc28
ElizPN <pankevych.liza@gmail.com>
2022-11-21 15:43:59 +0100

    add neme to output through []

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ import path from "path";
 export default {
   mode: "development",
   entry: {
-    bund1: ["./src/test1.js", "./src/test2.js"],
-    bund2: "./src/test3.js",
+    testOne: ["./src/test1.js", "./src/test2.js"],
+    testTwo: "./src/test3.js",
   },
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "[name].bund.[contenthash].js",
   },
 };
 
